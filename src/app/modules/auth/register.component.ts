27b78cd6837007861db38f5a6699ffbf5dd369f8
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2017-07-04 22:25:47 +0700

    auth

--- src/app/modules/auth/register.component.ts
+++ src/app/modules/auth/register.component.ts
@@ -18,16 +18,19 @@ export class RegisterComponent {
     }
 
     public actionRegister() {
+        this.AuthService.http.startLoad();
         this.AuthService.accRegister(this.fdata).subscribe(
             res => {
                 this.rSuccess = true;
                 this.rError = false;
                 this.alert = 'Register success!';
+                this.AuthService.http.endLoad();
             },
             error => {
                 this.rSuccess = false;
                 this.rError = true;
                 this.alert = error.json().message;
+                this.AuthService.http.endLoad();
             }
         );
     }
