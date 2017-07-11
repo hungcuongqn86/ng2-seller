d4a9b824014af732dc6b7a50f8ad8876756abb52
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2017-07-11 15:07:35 +0700

    layout

--- src/app/directive/color.directive.ts
+++ src/app/directive/color.directive.ts
@@ -15,13 +15,13 @@ export class ColorDirective implements OnInit {
     ngOnInit() {
         const prodDraw = SVG(this.el.nativeElement).size(20, 20);
         if (this.type === 'add') {
-            const gradient = prodDraw.gradient('linear', function (stop) {
-                stop.at(0, '#f06');
-                stop.at(1, '#0f9');
+            const gradient = prodDraw.gradient('radial', function (stop) {
+                stop.at(0, '#fff');
+                stop.at(1, '#337ab7');
             });
             prodDraw.circle(20).fill(gradient).stroke({color: '#000', opacity: 0.1, width: 1});
-            prodDraw.rect(8, 2).fill('#ffffff').move(6, 9);
-            prodDraw.rect(2, 8).fill('#ffffff').move(9, 6);
+            prodDraw.rect(10, 2).fill('#ffffff').move(5, 9);
+            prodDraw.rect(2, 10).fill('#ffffff').move(9, 5);
         } else {
             prodDraw.circle(20).fill(this.color).stroke({color: '#000', opacity: 0.1, width: 1});
         }
