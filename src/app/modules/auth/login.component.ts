8899de8d7065be81c8f6df44dafaed6ab7156b7d
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2017-08-16 10:17:00 +0700

    recaptcha

--- src/app/modules/auth/login.component.ts
+++ src/app/modules/auth/login.component.ts
@@ -3,61 +3,66 @@ import {Router} from '@angular/router';
 import {AuthService} from './auth.service';
 import {Observable} from 'rxjs/Rx';
 import {DsLib} from '../../lib/lib';
-import {moduleStart} from '../../lib/const';
+import {moduleStart, captcha_key} from '../../lib/const';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent {
-    rError;
-    alert;
-    fdata: any = JSON.parse('{"source": "general","email":"","password":""}');
+  rError;
+  alert;
+  fdata: any = {source: 'general', email: '', password: '', captcha: ''};
+  captcha_key = captcha_key;
 
-    constructor(private AuthService: AuthService, private router: Router) {
-        this.AuthService.http.canActive = [];
-    }
+  constructor(private AuthService: AuthService, private router: Router) {
+    this.AuthService.http.canActive = [];
+  }
 
-    public actionLogin() {
-        this.AuthService.http.startLoad();
-        this.AuthService.accLogin(this.fdata).subscribe(
-            res => {
-                DsLib.setToken(res);
-                this.createSession();
-            },
-            error => {
-                if (error.status === 401) {
-                    this.rError = true;
-                    this.alert = error.json().message;
-                } else {
-                    console.error(error.json().message);
-                    return Observable.throw(error);
-                }
-            }
-        );
-    }
+  public handleCorrectCaptcha(data) {
+    this.fdata.captcha = data;
+  }
 
-    private createSession() {
-        const tooken = DsLib.getToken().id;
-        this.AuthService._createSession(tooken).subscribe(
-            res => {
-                this.AuthService.http.profile = DsLib.getProfile();
-                this.AuthService.http.canActive = moduleStart;
-                this.AuthService.http.endLoad();
-                this.router.navigate(['/campaigns']);
-            },
-            error => {
-                this.AuthService.http.endLoad();
-                if (error.status === 401) {
-                    this.rError = true;
-                    this.alert = error.json().message;
-                } else {
-                    console.error(error.json().message);
-                    return Observable.throw(error);
-                }
-            }
-        );
-    }
+  public actionLogin() {
+    this.AuthService.http.startLoad();
+    this.AuthService.accLogin(this.fdata).subscribe(
+      res => {
+        DsLib.setToken(res);
+        this.createSession();
+      },
+      error => {
+        if (error.status === 401) {
+          this.rError = true;
+          this.alert = error.json().message;
+        } else {
+          console.error(error.json().message);
+          return Observable.throw(error);
+        }
+      }
+    );
+  }
+
+  private createSession() {
+    const tooken = DsLib.getToken().id;
+    this.AuthService._createSession(tooken).subscribe(
+      res => {
+        this.AuthService.http.profile = DsLib.getProfile();
+        this.AuthService.http.canActive = moduleStart;
+        this.AuthService.http.endLoad();
+        this.router.navigate(['/campaigns']);
+      },
+      error => {
+        this.AuthService.http.endLoad();
+        if (error.status === 401) {
+          this.rError = true;
+          this.alert = error.json().message;
+        } else {
+          console.error(error.json().message);
+          return Observable.throw(error);
+        }
+      }
+    );
+  }
 }
